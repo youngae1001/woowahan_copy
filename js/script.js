46bfc097334678ccb4706aed14af90813e00418c
youngae1001 <youngjenny@naver.com>
2022-05-05 23:05:50 +0900

    220505_수정

--- js/script.js
+++ js/script.js
@@ -1,13 +1,39 @@
 $(document).ready(function(){
 
     $(".top_bar_wrap > .top_bar > .nav_box > ul > li > .sub_top_bar_wrap > .sub_top_bar > li > a").mouseover(function(){
-        $(".top_bar_wrap > .top_bar > .nav_box > ul > li > .sub_top_bar_wrap > .sub_top_bar > li").removeClass("active")
+        $(".top_bar_wrap > .top_bar > .nav_box > ul > li > .sub_top_bar_wrap > .sub_top_bar > li > a").removeClass("active")
     })
     $(".top_bar_wrap > .top_bar > .nav_box > ul > li > .sub_top_bar_wrap > .sub_top_bar > li > a").mouseleave(function(){
-        $(".top_bar_wrap > .top_bar > .nav_box > ul > li > .sub_top_bar_wrap > .sub_top_bar > li:first-child").addClass("active")
+        $(".top_bar_wrap > .top_bar > .nav_box > ul > li > .sub_top_bar_wrap > .sub_top_bar > li:first-child > a").addClass("active")
     })
 
+    $('.slider').on('init', function(event, slick){
+        $(".content").eq(1).addClass("active")
+    });
 
+    $(".slider").slick({
+        autoplay : true ,
+        autoplaySpeed : 5000 ,
+        dots: true ,
+        draggable : true ,
+        infinite: true ,
+    })
+
+    $('.slider').on('beforeChange', function(event, slick, currentSlide, nextSlide){
+        $(".content").removeClass("active")
+    });
+
+    $('.slider').on('afterChange', function(event, slick, currentSlide){
+        $(".content").eq(currentSlide+1).addClass("active")
+    });
+
+    for(var i = 1; i <= 10; i++) {
+        var letter = $(".main_visual > .slider_wrap > .slider .content > a > .txt > p:nth-child("+i+")");
+        var delay = 0.2 * i;
+        letter.css({
+            "transition-delay" : delay + "s",
+        })
+    }
 
     // 끝
 })
